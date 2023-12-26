c3e46b352cb8205904f57d8545ec50287ca99145
Mohammedsekk <seksimo2001@gmail.com>
2023-12-26 13:03:24 +0200

    time to get some data

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
-"""modul doc"""
+""" module doc """
 from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
-app= Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
 def hello():
-    """def doc"""
+    """ def doc """
     return "Hello HBNB!"
-@app.route("/hbnb",strict_slashes=False)
-def hbhb():
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
     """ def doc """
     return "HBNB"
-@app.route("/c/<text>",strict_slashes=False)
+
+
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ def doc """
-    return 'C {}'.format(text.replace("_"," "))
+    return 'c {}'.format(text.replace("_", " "))
+
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>",strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text):
-     """ def doc """
-     return 'Python {}'.format(text.replace("_"," "))
-    
-@app.route("/number/<int:n>",strict_slashes=False)
+    """ def doc """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    
-        return "{} is a number".format(n)
-@app.route("/number_template/<int:n>",strict_slashes=False)
-def number_template(n):
-     return render_template('5-number.html', number=n)
+    """ def doc """
+    return '{} is a number'.format(n)
+
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """ def doc """
@@ -41,15 +48,19 @@ def number_odd_or_even(n):
         p = 'odd'
     return render_template('6-number_odd_or_even.html', number=n, parity=p)
 
-@app.route("/states_list",strict_slashes=False)
+
+@app.route('/states_list', strict_slashes=False)
 def states_list():
     """ def doc """
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
-  
-  
+
+
 @app.teardown_appcontext
 def close(error):
-     storage.close()
+    """ def doc """
+    storage.close()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
